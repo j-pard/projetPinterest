472c87e69c568a22dd9a7329008e739e23b0fbb0
J-Pard <pardons.jonathan@gmail.com>
2020-06-23 10:29:04 +0200

    database

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -477,6 +477,7 @@ class ComposerStaticInit0170ee68bb4c8673770b462a7a91d8d9
     public static $classMap = array (
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
+        'App\\Http\\Controllers\\ArticleController' => __DIR__ . '/../..' . '/app/Http/Controllers/ArticleController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
