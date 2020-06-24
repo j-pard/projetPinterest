c8d287487db500feb504a24313b2e75bee432a85
j-pard <pardons.jonathan@gmail.com>
2020-06-24 13:47:43 +0200

    avatar

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
       <h1>Votre profil</h1>
-
+      <figure>
+      <img src="{{ Auth::user()->avatar }}" alt="">
+      </figure>
       <h3>Nom : {{ Auth::user()->name }}</h3>
       <h3>Email : {{ Auth::user()->email }}</h3>
 @endsection
