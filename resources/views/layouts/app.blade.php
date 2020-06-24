4c42f405ea4087a8757cf8a48a56315e608f3bd9
j-pard <pardons.jonathan@gmail.com>
2020-06-24 13:58:51 +0200

    My profil & gitignore update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
+                                    <a class="dropdown-item" href="/profil">Mon Profil</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
