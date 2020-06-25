e93bab066b37d8e1e36da4b6cbd8981b8add4f76
Corentin <corentin.colart@gmail.com>
2020-06-25 15:44:49 +0200

    update profile.scss

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-      <h1>Votre profil</h1>
-
       <section class="container">
-            <figure>
+            <!-- <figure>
                   <img src="{{ Auth::user()->avatar }}" alt="">
             </figure>
             <h3>Nom : {{ Auth::user()->pseudo }}</h3>
-            <h3>Email : {{ Auth::user()->email }}</h3>
+            <h3>Email : {{ Auth::user()->email }}</h3> -->
+
+<div class="row">
+    <div class="avatar">
+      <img src="{{ Auth::user()->avatar }}"  alt="User avatar">
+    </div>
+    <div class="username">
+      <blockquote>
+        <h3>{{ Auth::user()->pseudo }}</h3>
+        <small><cite title="name">{{ Auth::user()->firstname }}</cite></small>
+      </blockquote>
+    </div>
+  </div>
       </section>
       
 
