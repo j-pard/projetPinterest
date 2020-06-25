eedeaf35367d7817a1050a239c88274d260a5759
J-Pard <pardons.jonathan@gmail.com>
2020-06-25 14:09:14 +0200

    Collection in profil

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,11 +2,30 @@
 
 @section('content')
       <h1>Votre profil</h1>
-      <figure>
-      <img src="{{ Auth::user()->avatar }}" alt="">
-      </figure>
-      <h3>Nom : {{ Auth::user()->pseudo }}</h3>
-      <h3>Email : {{ Auth::user()->email }}</h3>
 
-      <a href="/profil/edit" class="btn btn-info" role="button">Modifier profil</a>
+      <section class="container">
+            <figure>
+                  <img src="{{ Auth::user()->avatar }}" alt="">
+            </figure>
+            <h3>Nom : {{ Auth::user()->pseudo }}</h3>
+            <h3>Email : {{ Auth::user()->email }}</h3>
+
+            <a href="/profil/edit" class="btn btn-info" role="button">Modifier profil</a>
+      </section>
+      
+
+      <section class="container ">
+            <div class="gallery" id="gallery">
+            @php
+                  $collection = DB::table('articles')->where('author', Auth::user()->id)->get();
+            @endphp
+             
+            @foreach ($collection as $item)
+            <div class="mb-3 pics animation all 1">
+                  <img class="img-fluid" src = "{{$item->image}}" alt="{{$item->title}}"> 
+                </div>
+            @endforeach
+            </div>
+      </section>
 @endsection
+
