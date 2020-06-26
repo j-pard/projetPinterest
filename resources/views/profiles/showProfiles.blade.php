a9fd7a1657d92f4fb842c731d482b525ad0640a1
Corentin <corentin.colart@gmail.com>
2020-06-26 15:47:44 +0200

    Showprofiles update

--- resources/views/profiles/showProfiles.blade.php
+++ resources/views/profiles/showProfiles.blade.php
@@ -2,22 +2,58 @@
 
 @section('content')
 
-      <section class="container">
-            <figure>
-                  <img src="/{{ $user->avatar }}" alt="">
-            </figure>
-            <h3>Nom : {{ $user->pseudo }}</h3>
-            <h3>Email : {{ $user->email }}</h3>
+@php
+    $author = DB::table('users')->where('id', $article->author)->value('pseudo');
+    $author_id = DB::table('users')->where('id', $article->author)->value('id');
 
+    $author_avatar = DB::table('users')->where('id', $author->author)->value('avatar');
+
+    $originalDate = $article->updated_at;
+    $newDate = date("d-m-Y", strtotime($originalDate));
+
+    $collection = DB::table('articles')->where('author', $user->id)->get();
+
+    $isFollowed = DB::table('user_follower')
+                                        ->where('follower_id', Auth::user()->id)
+                                        ->where('following_id', $author_id)
+                                        ->exists();
+
+                                    if($isFollowed) {
+                                        $btnValue = "Unfollow";
+                                    }
+                                    else {
+                                        $btnValue = "Follow"; 
+                                    }
+@endphp
+
+<section class="container">
+
+  
+      <div class="coverimage">
+            <img src="{{ Auth::user()->cover }}"  alt="User cover">
+          </div>
+  
+      <div class="row profilavatar">
+
+                  
+            <div class="avatar">
+                  <img src="/{{$author_avatar}}"  alt="User avatar">
+            </div>    
             
-      </section>
+            
+
+            <div class="username">
+                  <blockquote>
+                  <h3>{{ Auth::user()->pseudo }}</h3>
+                  <small><cite title="name">{{ Auth::user()->firstname }}</cite></small>
+                  </blockquote>
+            </div>
+      </div>
+</section>
       
 
       <section class="container ">
             <div class="gallery" id="gallery">
-            @php
-                  $collection = DB::table('articles')->where('author', $user->id)->get();
-            @endphp
              
             @foreach ($collection as $article)
             <div class="mb-3 pics animation all 1">
